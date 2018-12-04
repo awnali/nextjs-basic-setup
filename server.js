6d766d71deb5258916c153ff3440804516c68be7
awn <awn@turnlunchon.com>
2018-12-04 10:04:13 +0400

    dynamic routering bug fix

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-const express = require("express");
+// server.js
 const next = require("next");
-const routes = require("./routes");
-
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const routes = require("./routes"); // <- your routes file
+const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 
+// With express
+const express = require("express");
 app.prepare().then(() => {
-  const server = express();
-  server.use(handler);
-
-  server.get("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(3000, err => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:3000");
-  });
+  express()
+    .use(handler) // <- this line is important
+    .listen(3000);
 });
