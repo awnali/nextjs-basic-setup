a7c852838fa300ec423c08a105a7ab6bf480cd79
awn <awn@turnlunchon.com>
2018-12-06 12:22:43 +0400

    move polyfill.js to the bottom of body

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,11 @@ export default class MyDocument extends Document {
           <title>My next js app</title>
           <style>{`body { margin: 0 } /* custom! */`}</style>
           <link rel="stylesheet" href="/static/app.css" />
-          <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
         </Head>
         <body className="custom_class">
           <Main />
           <NextScript />
+          <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
           <script
             async
             src="https://cloud.lunchon.ae/static/js/checkoutkit.js"
